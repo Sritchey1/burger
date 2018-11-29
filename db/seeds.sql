0d2379bbbbead7521780994abbafe5603bc93e41
Scott Ritchey <scottritchey@Scotts-MacBook-Air-2.local>
2018-11-29 17:11:34 -0500

    incomplete

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+/* Insert data */
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Southwest Burger');
+INSERT INTO burgers (burger_name) VALUES ('Vegan Burger');
+INSERT INTO burgers (burger_name) VALUES ('Turkey Burger');
